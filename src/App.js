0178ef2b28226f36d58343e28764723de262aa5c
unknown <nzira0365@gmail.com>
2022-10-28 11:22:10 +0800

    adding props children

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import { Route,Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import FavoritesPage from "./pages/Favourite";
 import NewMeetupPage from "./pages/NewMeetup";
 import AllMeetupsPage from "./pages/AllMeetups";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
+// import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   //localhost:3000/ - our domain
   // my-page.com/
   return (
-    <div>
-      <MainNavigation/>
+    <Layout>
       <Switch>
-      <Route path="/" exact>
-        <AllMeetupsPage />
-      </Route>
-      <Route path="/new-meetup">
-        <NewMeetupPage />
-      </Route>
-      <Route path="/favorites">
-        <FavoritesPage />
-      </Route>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
