7d614b10f322b542087c575c42833e65ffa03d0c
unknown <nzira0365@gmail.com>
2022-11-10 11:39:24 +0800

    sending http request & firebase database

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 
 import Card from '../ui/Card';
 import classes from './NewMeetupForm.module.css';
-function NewMeetupForm() {
+function NewMeetupForm(props) {
     const titleInputRef = useRef();
     const imageInputRef = useRef();
     const addressInputRef = useRef();
@@ -24,7 +24,7 @@ function NewMeetupForm() {
             address: enteredAdress,
             description: enteredDescription
         }
-        console.log(meetupData);
+        props.onAddMeetup(meetupData);
     }
     return <Card>
         <form className={classes.form} onSubmit={submitHandler}>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,22 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData){
+    fetch('https://react-getting-started-4d548-default-rtdb.firebaseio.com/meetups.json' ,
+    {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    ); //firebase url to send a api request
+
+  }
+
     return <section>
       <h1> Add New Meetup</h1>
-      <NewMeetupForm/>
+      <NewMeetupForm onAddMeetup = {addMeetupHandler}/>
     </section>;
   }
   
